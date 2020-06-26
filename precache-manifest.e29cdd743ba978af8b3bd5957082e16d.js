8110e4cfd6581b9caeca9fb26e12b6893f1e5e98
Jake Scudder <scudsterr@gmail.com>
2020-06-26 19:05:15 -0400

    Updates

---
+++ precache-manifest.e29cdd743ba978af8b3bd5957082e16d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344d27c74828d575b468135b3cb110d0",
+    "url": "/tarot-api/index.html"
+  },
+  {
+    "revision": "e96f4b9873803c97f307",
+    "url": "/tarot-api/static/css/main.8595d79c.chunk.css"
+  },
+  {
+    "revision": "08435f7f3e25ce8995d8",
+    "url": "/tarot-api/static/js/2.df5d4a27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tarot-api/static/js/2.df5d4a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96f4b9873803c97f307",
+    "url": "/tarot-api/static/js/main.ca28d122.chunk.js"
+  },
+  {
+    "revision": "b2a70cc6cf18f0bf0ad0",
+    "url": "/tarot-api/static/js/runtime-main.a59d030a.js"
+  },
+  {
+    "revision": "5346ee6af73c0065653cbf4163baedd9",
+    "url": "/tarot-api/static/media/galaxy.5346ee6a.svg"
+  }
+]);
